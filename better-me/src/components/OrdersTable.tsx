e6922f66b2dfaee78569f9c22a65d983287102dd
respekttt <solianyk.denis@lll.kpi.ua>
2026-02-24 23:26:46 +0200

    components

---
+++ better-me/src/components/OrdersTable.tsx
@@ -0,0 +1,28 @@
+export function OrderTable(){
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Order id and item</td>
+                        <td>Location</td>
+                        <td>Subtotal</td>
+                        <td>Tax Rate</td>
+                        <td>Tax Amount</td>
+                        <td>Total</td>
+                        <td>Info</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr></tr>
+                    <tr></tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
